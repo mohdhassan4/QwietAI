3173cbc6c71aed3886500b9eb122e2747278b365
Harness Auto-Remediation <remediation@harness.io>
2026-08-26 22:28:20 +0000

    vuln-fix: Add Secure and SameSite=Strict to cookies (STO-pEcSUuwrNzQOBhtiyb87US)
    Add Secure attribute and upgrade SameSite to Strict on document.cookie creation and clearing in CachePoisoningCommon.js.
    
    Weakness: CWE-1126
    Severity: Low
    Detection: Tool
    Signed-off-by: Harness Auto-Remediation <remediation@harness.io>
    Co-authored-by: Harness Auto-Remediation <remediation@harness.io>

--- src/main/resources/static/templates/CachePoisoning/Common/CachePoisoningCommon.js
+++ src/main/resources/static/templates/CachePoisoning/Common/CachePoisoningCommon.js
@@ -48,10 +48,10 @@ export function getHeadersWithForwardedHost(inputId = "forwardedHostInput") {
 
 export function setDemoUserCookie(value) {
   if (value) {
-    document.cookie = `demo_user=${value}; path=/; SameSite=Lax`;
+    document.cookie = `demo_user=${value}; path=/; Secure; SameSite=Strict`;
   } else {
     document.cookie =
-      "demo_user=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+      "demo_user=; path=/; Secure; SameSite=Strict; expires=Thu, 01 Jan 1970 00:00:00 GMT";
   }
 }
 
